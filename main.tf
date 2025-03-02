04942031aeb1f6e600d234d325f453138026a5fc
Akshara Sadheesh <94154731+sharanair29@users.noreply.github.com>
2025-03-02 14:35:02 +0800

    poc aws

--- main.tf
+++ main.tf
@@ -1,19 +1,47 @@
-provider "docker" {}
-
-resource "docker_image" "sql_server" {
-  name = "mcr.microsoft.com/mssql/server:2019-latest"
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "docker_container" "sql" {
-  name  = "sql_container"
-  image = docker_image.sql_server.image_id
-  env = [
-    "ACCEPT_EULA=Y",
-    "SA_PASSWORD=StrongP@ssw0rd",
-    "MSSQL_PID=Developer"
-  ]
-  ports {
-    internal = 1433
-    external = 1433
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 5985
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Windows Server 2019 AMI
+  instance_type = "t2.medium"
+  subnet_id     = aws_subnet.subnet.id
+  security_groups = [aws_security_group.sg.name]
+  key_name      = "my-key-pair"
+
+  tags = {
+    Name = "sqlserver-ec2"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.ec2.public_ip
 }
